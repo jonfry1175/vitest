662dee78a84dfd893c6816987e0157ed7a8ac9c7
Hebron <jsfast2@gmail.com>
2024-07-22 15:22:02 +0700

    update dashboard

--- src/pages/dashboard/DashboardPage.jsx
+++ src/pages/dashboard/DashboardPage.jsx
@@ -10,10 +10,23 @@ const DashboardPage = () => {
           <Sidebar />
         </div>
         <div className="col">
-          <div className="text-center">
+          <div>
+            <div className="text-center display-2 margin: 1rem margin-bottom: 5rem">
             <h1>Dashboard Page</h1>
-            
-            
+            </div>
+            <br/>
+            <br/>
+            <br/>
+            <div className='text-center'>
+            <h1 className='text-primary'>Welcome Admin!</h1>
+            <ul className='list-unstyled text-left'>
+              <li>Kemudahan pembayaran</li>
+              <li>Kemudahan dalam mengelola laporan keuangan</li>
+              <li>Kemudahan Marketing</li>
+              <li>Kelancaran bertransaksi</li>
+              <li>Kemudahan penulisan produk serta data kustomer hanya di 1 perangkat</li>
+            </ul>
+            </div>
           </div>
         </div>
       </div>
